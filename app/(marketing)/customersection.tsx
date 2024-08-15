8e7c85f86b99c738a56becfc209f265f88979b5a
Đào Duy Thái - HE176315 <daoduythai.business@gmail.com>
2024-08-15 15:36:00 +0700

    add customer section skeleton

--- app/(marketing)/customersection.tsx
+++ app/(marketing)/customersection.tsx
@@ -7,8 +7,9 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel"
 import Image from "next/image";
+import { Skeleton } from "@/components/ui/skeleton"
 
-const customerResponses = [
+const customerResponses: Array<any> | null = [
     {
         id: 1,
         name: "John D.",
@@ -69,48 +70,90 @@ const customerResponses = [
 
 
 export default function CustomerSection() {
+    if (customerResponses == null) return <CustomerSectionSkeleton />;
+
     return (
         <section className="bg-neutral-100 py-10">
             <div className="w-full h-full container my-10">
                 <div className="text-2xl md:text-4xl font-bold lg:text-left my-3">
                     Customers&nbsp;
                 </div>
-                <div className="text-md md:text-xl font-medium lg:text-left ">What people using InSync are saying?</div>
+                <div className="text-md md:text-xl font-medium lg:text-left">
+                    What people using InSync are saying?
+                </div>
             </div>
             <div className="container px-14">
-                <Carousel
-                    opts={{ align: "start", }}
-                    className="w-full h-full"
-                >
-                    <CarouselContent >
-                        {
-                            customerResponses?.map((cr) => (
-                                <CarouselItem key={cr.id} className="md:basis-1/4 lg:basis-1/5">
-                                    <div className="p-1">
-                                        <Card>
-                                            <CardContent className="flex aspect-square items-center justify-center p-6">
-                                                <span className="text-xl font-normal">“{cr.review}”</span>
-                                            </CardContent>
-                                            <CardHeader>
-                                                <div className="flex gap-2 align-middle">
-                                                    <Image className="rounded-full h-fit" src={cr.image} width={40} height={40} alt="hero" />
-                                                    <div>
-                                                        <CardTitle>{cr.name}</CardTitle>
-                                                        <CardDescription>{cr.jobTitle}</CardDescription>
-                                                    </div>
+                <Carousel opts={{ align: "start" }} className="w-full h-full">
+                    <CarouselContent>
+                        {customerResponses?.map((cr) => (
+                            <CarouselItem key={cr.id} className="md:basis-1/4 lg:basis-1/5">
+                                <div className="p-1">
+                                    <Card>
+                                        <CardContent className="flex aspect-square items-center justify-center p-6">
+                                            <span className="text-xl font-normal">“{cr.review}”</span>
+                                        </CardContent>
+                                        <CardHeader>
+                                            <div className="flex gap-2 align-middle">
+                                                <Image className="rounded-full h-fit" src={cr.image} width={40} height={40} alt={cr.name} />
+                                                <div>
+                                                    <CardTitle>{cr.name}</CardTitle>
+                                                    <CardDescription>{cr.jobTitle}</CardDescription>
                                                 </div>
-                                            </CardHeader>
-                                        </Card>
-                                    </div>
-                                </CarouselItem>
-                            ))
-                        }
+                                            </div>
+                                        </CardHeader>
+                                    </Card>
+                                </div>
+                            </CarouselItem>
+                        ))}
                     </CarouselContent>
                     <CarouselPrevious />
-                    <CarouselNext/>
+                    <CarouselNext />
                 </Carousel>
             </div>
-
         </section>
     );
 }
+
+
+export const CustomerSectionSkeleton = function CustomerSectionSkeleton() {
+    return (
+        <section className="bg-neutral-100 py-10">
+            <div className="w-full h-full container my-10">
+                <div className="text-2xl md:text-4xl font-bold lg:text-left my-3">
+                    Customers&nbsp;
+                </div>
+                <div className="text-md md:text-xl font-medium lg:text-left">
+                    What people using InSync are saying?
+                </div>
+            </div>
+            <div className="container px-14">
+                <Carousel opts={{ align: "start" }} className="w-full h-full">
+                    <CarouselContent>
+                        {Array(5).fill(null).map((_, index) => (
+                            <CarouselItem key={index} className="md:basis-1/4 lg:basis-1/5">
+                                <div className="p-1">
+                                    <Card>
+                                        <CardContent className="flex aspect-square items-center justify-center p-6">
+                                            <Skeleton className="h-20 w-20 rounded-full" />
+                                        </CardContent>
+                                        <CardHeader>
+                                            <div className="flex gap-2 align-middle">
+                                                <Skeleton className="rounded-full h-10 w-10" />
+                                                <div className="flex flex-col">
+                                                    <Skeleton className="h-6 w-24" />
+                                                    <Skeleton className="h-4 w-32 mt-1" />
+                                                </div>
+                                            </div>
+                                        </CardHeader>
+                                    </Card>
+                                </div>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
+        </section>
+    );
+};
