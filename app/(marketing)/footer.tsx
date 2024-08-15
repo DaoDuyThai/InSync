dd0a8c4cf5555cd3cba0e4316a415c95e1c7d560
Đào Duy Thái - HE176315 <daoduythai.business@gmail.com>
2024-08-15 17:03:02 +0700

    adjust links in footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -11,24 +11,28 @@ export const Footer = () => {
             <div className="container p-10 w-full">
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 pb-5">
                     <div className="flex items-start gap-x-3">
-                        <Image src="/logo.svg" alt="Logo" width={40} height={40} />
-                        <span className="font-extrabold text-2xl tracking-wide text-center">InSync</span>
+                        <div className="flex items-center">
+                            <Image src="/logo.svg" alt="Logo" width={40} height={40} />
+                            <div className="font-extrabold text-2xl tracking-wide text-center">InSync</div>
+                        </div>
                     </div>
                     <div className="flex flex-col gap-4">
-                        <div className="font-bold pb-2">RESOURCES</div>
-                        <Link href="/client">InSync Client</Link>
-                        <Link href="/tutorials">Tutorials</Link>
+                        <div className="font-bold pb-2">GETTING STARTED</div>
+                        <Link href="/introduction">Introduction</Link>
+                        <Link href="/docs/installation">Installation</Link>
+                        <Link href="/docs/examples">Examples</Link>
                     </div>
                     <div className="flex flex-col gap-4">
                         <div className="font-bold pb-2">ABOUT</div>
-                        <Link href="/pricing">Pricing</Link>
-                        <Link href="/aboutus">About InSync</Link>
-                        <Link href="/faq">Frequent Asked Questions</Link>
+                        <Link href="/about">About InSync</Link>
+                        <Link href="/faq">FAQs</Link>
+                        <Link href="/terms">Term of Service</Link>
+                        <Link href="/privacypolicy">Privacy Policy</Link>
                     </div>
                     <div className="flex flex-col gap-4">
-                        <div className="font-bold pb-2">LEGAL</div>
-                        <Link href="/privacypolicy">Privacy Policy</Link>
-                        <Link href="/terms">Terms</Link>
+                        <div className="font-bold pb-2">SERVICE</div>
+                        <Link href="/price">Pricing</Link>
+                        <Link href="/docs">Documentation</Link>
                     </div>
                     <div className="col-span-full md:col-span-2 lg:col-span-1 flex flex-col gap-4">
                         <div className="font-bold pb-2 w-full text-center">CONTACT US</div>
