36d1d1aa9a0b35ce228455860acf7f6c36ee99a3
Dương Thành Luân <103130587+LuanDTH0104@users.noreply.github.com>
2024-09-08 14:56:15 +0700

    Update Multiple Crop with js canvas

--- app/image/page.tsx
+++ app/image/page.tsx
@@ -1,39 +1,180 @@
 'use client'
+import { cva } from "class-variance-authority";
+import { log } from "console";
 import { useEffect, useRef } from "react";
-
+import { useState, useCallback } from 'react'
+import Cropper from 'react-easy-crop'
 export default function ImagePage() {
-     // Create a ref for the canvas element
-     const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const croppedRef = useRef<HTMLCanvasElement>(null);
+    const mount = useRef(false);
+    const canvasAreaRef = useRef<HTMLDivElement>(null)
 
-     useEffect(() => {
-         // Get the canvas element and its 2D context
-         const canvas = canvasRef.current;
-         if (canvas) {
-             const ctx = canvas.getContext('2d');
-             if (ctx) {
-                 // Set the rectangle properties
-                 const x = 50; // X coordinate of the top-left corner
-                 const y = 50; // Y coordinate of the top-left corner
-                 const width = 50; // Width of the rectangle
-                 const height = 50; // Height of the rectangle
- 
-                 // Set the fill color
-                 ctx.fillStyle = 'blue'; // Color for the rectangle
- 
-                 // Draw the rectangle
-                 ctx.fillRect(x, y, width, height);
-             }
-         }
-     }, []); // Empty dependency array ensures this runs once after the initial render
- 
-     return (
-         <div>
-             <canvas
-                 ref={canvasRef}
-                 width={500}
-                 height={500}
-                 style={{ border: '1px solid black' }}
-             />
-         </div>
-     );
+    useEffect(() => {
+
+        if (mount.current === false) {
+            // Get the canvas and its context
+            const canvas = canvasRef.current;
+            const ctx = canvas?.getContext('2d');
+            const croppedCanvas = croppedRef.current;
+            const croppedCtx = croppedCanvas?.getContext('2d');
+            const canvasArea = canvasAreaRef.current;
+            let selectedRectIndex;
+            // Create a new image object
+            const img = new Image();
+            img.src = 'https://static.chotot.com/storage/chotot-kinhnghiem/c2c/2019/10/nuoi-meo-can-gi-0-1024x713.jpg'; // Update with the path to your image
+
+            // Array to store rectangles
+            const rectangles: Array<any> = [];
+
+            // Variables to track rectangle drawing
+            let startX: number, startY: number;
+            let isDrawing = false;
+
+            // Draw the image on the canvas once it's loaded
+            img.crossOrigin = "Anonymous";
+            img.onload = function () {
+                drawImageAndRectangles(ctx, canvas, img, rectangles);
+            };
+
+
+            // Function to start drawing
+            canvas?.addEventListener('mousedown', (e) => {
+                const rect = canvas.getBoundingClientRect();
+                startX = e.clientX - rect.left;
+                startY = e.clientY - rect.top;
+                isDrawing = true;
+            });
+
+            // Function to draw the rectangle while mouse is moving
+            canvas?.addEventListener('mousemove', (e) => {
+                if (!isDrawing) return;
+
+                // Calculate the current mouse position
+                const rect = canvas.getBoundingClientRect();
+                const currentX = e.clientX - rect.left;
+                const currentY = e.clientY - rect.top;
+
+                // Calculate width and height of the rectangle
+                const width = currentX - startX;
+                const height = currentY - startY;
+
+                // Clear the canvas and redraw the image and existing rectangles
+                drawImageAndRectangles(ctx, canvas, img, rectangles);
+
+                // Draw the new rectangle being created
+                if (ctx) {
+                    ctx.beginPath();
+                    ctx.rect(startX, startY, width, height);
+                    ctx.lineWidth = 3;
+                    ctx.strokeStyle = 'white';
+                    ctx.stroke();
+                }
+            });
+
+            // Function to finish drawing the rectangle
+            canvas?.addEventListener('mouseup', (e) => {
+
+                if (isDrawing) {
+                    const rect = canvas.getBoundingClientRect();
+                    const currentX = e.clientX - rect.left;
+                    const currentY = e.clientY - rect.top;
+
+                    // Calculate width and height of the rectangle
+                    const width = currentX - startX;
+                    const height = currentY - startY;
+
+                    // Add the new rectangle to the array
+                    rectangles.push({ startX, startY, width, height });
+                    console.log(rectangles);
+
+
+                    // Reset drawing state
+                    isDrawing = false;
+
+                    // Redraw everything
+                    drawImageAndRectangles(ctx, canvas, img, rectangles);
+                }
+            });
+
+            // Add an event listener to crop the most recent rectangle
+            document.addEventListener('keydown', (e) => {
+                if (e.key === 'Enter') {
+                    let length = rectangles.length
+                    const rect = rectangles[length - 1]
+                    let canvas = document.createElement("canvas");
+                    let canvasContext = canvas.getContext('2d');
+                    let imageData = ctx?.getImageData(rect.startX, rect.startY, rect.width, rect.height);
+                    canvas.width = rect.width;
+                    canvas.height = rect.height;
+                    if (imageData) {
+                        canvasContext?.putImageData(imageData, 0, 0);
+                    }
+                    canvasArea?.appendChild(canvas);
+
+
+                }
+            });
+
+
+            canvas?.addEventListener('click', (e) => {
+                const rect = canvas.getBoundingClientRect();
+                const mouseX = e.clientX - rect.left;
+                const mouseY = e.clientY - rect.top;
+
+                selectedRectIndex = null; // Reset selected rectangle
+                rectangles.forEach((rect, index) => {
+                    if (mouseX >= rect.startX && mouseX < rect.startX + rect.width &&
+                        mouseY >= rect.startY && mouseY < rect.startY + rect.height) {
+                        selectedRectIndex = index;
+                    }
+                });
+
+                if (selectedRectIndex !== null) {
+                    // Confirm removal of the selected rectangle
+                    const confirmed = confirm('Do you want to remove the selected rectangle?');
+                    if (confirmed) {
+                        // Remove the selected rectangle
+                        rectangles.splice(selectedRectIndex, 1);
+                        selectedRectIndex = null;
+                        drawImageAndRectangles(ctx, canvas, img, rectangles);
+                    }
+                }
+            });
+
+        }
+
+        // Function to draw image and rectangles
+        function drawImageAndRectangles(ctx: any, canvas: any, img: any, rectangles: Array<any>) {
+            if (ctx && canvas) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+                // Draw all rectangles
+                rectangles.forEach(rect => {
+                    ctx.beginPath();
+                    ctx.rect(rect.startX, rect.startY, rect.width, rect.height);
+                    ctx.strokeStyle = 'white';
+                    ctx.stroke();
+                });
+            }
+        }
+
+
+        return () => {
+            mount.current = true;
+        }
+
+
+
+
+    }, [])
+
+    return (
+        <>
+            <canvas ref={canvasRef} width="900" height="600" className="border-2 border-red-300"></canvas>
+            <div ref={canvasAreaRef}></div>
+        </>
+
+    )
 }
