ec1031a62483e8b00eb4165dc51b16fb745770b8
Đào Duy Thái - HE176315 <daoduythai.business@gmail.com>
2024-09-26 13:21:55 +0700

    adjust favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>InSync</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="InSync" />
+        <meta name="author" content="InSync Team" />
+        <meta name="robots" content="noindex, nofollow" />
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      </head>
       <body className={inter.className}>
         <Suspense fallback={<Loading />}>
           <ClerkProvider>
