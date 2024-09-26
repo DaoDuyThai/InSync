9a7b205b879e5c91af1af0541c124947169472fe
Dương Thành Luân <103130587+LuanDTH0104@users.noreply.github.com>
2024-09-26 13:05:09 +0700

    update total asset

--- app/assets/page.tsx
+++ app/assets/page.tsx
@@ -1,16 +1,18 @@
 'use client'
 
+import { log } from "console";
+import { CalendarFoldIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useEffect, useRef } from "react";
 
 export default function ImagePage() {
     const mount = useRef(false);
+    const router = useRouter();
 
     useEffect(() => {
-        if(mount.current === false) {
+        if (mount.current === false) {
             // Set page title
             document.title = "InSync - Assets Management";
-            
-
         }
 
         return () => {
@@ -18,7 +20,24 @@ export default function ImagePage() {
         }
     });
 
+    const handleClick = (id: string) => {
+        console.log("clicked");
+        router?.push(`/assets/${id}`);
+    }
+
     return (
-       <div className="p-10">Image Page</div>
+        <div className="container">
+            <div className="text-xl bg-[#f2f6fa] py-2 px-4">
+                <CalendarFoldIcon size="24" className="inline-block" />
+                <span className="mx-2">20/09/2023</span>
+            </div>
+            <div className="flex flex-wrap gap-1">
+                {[...Array(10)].map((_, i) => (
+                    <div key={i} className="border-2 border-white">
+                        <img onClick={() => handleClick(i.toString())} className="w-[100px] hover:animate-pulse" src="https://st.quantrimang.com/photos/image/2018/02/26/bat-thong-bao-khi-chia-se-hinh-anh-1.jpg" alt="anh-nen-dien-thoai" />
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
