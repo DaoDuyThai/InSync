465134c807f874ab80e4db4c9cd16542a45c0bc7
Đào Duy Thái - HE176315 <daoduythai.business@gmail.com>
2024-09-26 13:22:45 +0700

    adjust description

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <title>InSync</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="InSync" />
+        <meta name="description" content="Advanced Services for Enhanced Mobile Automation" />
         <meta name="author" content="InSync Team" />
         <meta name="robots" content="noindex, nofollow" />
         <link rel="icon" href="/favicon/favicon.ico" />
