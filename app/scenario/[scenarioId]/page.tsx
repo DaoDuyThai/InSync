d6668887f7608581ae5a6622614dc7edf0160e2d
Dương Thành Luân <103130587+LuanDTH0104@users.noreply.github.com>
2024-10-07 16:54:11 +0700

    Itergrate scenario

--- app/scenario/[scenarioId]/page.tsx
+++ app/scenario/[scenarioId]/page.tsx
@@ -1,7 +1,12 @@
-// import { Canvas } from "./_components/canvas";
-// import { Room } from "@/components/room";
-import { Suspense } from "react";
+'use client'
+import { Suspense, useEffect, useRef } from "react";
 import { Loading } from "./_components/loading";
+import * as Blockly from 'blockly';
+import { blocks } from './_components/blocks/json';
+import { save, load } from './_components/serialization';
+import { toolbox } from './_components/toolbox';
+import { jsonGenerator } from './_components/generators/json';
+import '@/CSS/blockly.css';
 
 interface ScenarioIdPageProps {
     params: {
@@ -13,10 +18,73 @@ interface ScenarioIdPageProps {
 const ScenarioIdPage = ({
     params
 }: ScenarioIdPageProps) => {
+    const mount = useRef(false);
+
+    useEffect(() => {
+        if (mount.current == false) {
+
+            // Register the blocks and generator with Blockly
+            Blockly.common.defineBlocks(blocks);
+
+            // Set up UI elements and inject Blockly
+            const codeDiv = document.getElementById('generatedCode')?.firstChild;
+            const blocklyDiv = document.getElementById('blocklyDiv');
+            if (blocklyDiv && codeDiv) {
+                const ws = Blockly.inject(blocklyDiv, { toolbox });
+
+                /// This function resets the code div and shows the
+                // generated code from the workspace.
+                const runCode = () => {
+                    const code = jsonGenerator.workspaceToCode(ws);
+                    (codeDiv as HTMLElement).innerText = code;
+                };
+
+                // Load the initial state from storage and run the code.
+                load(ws);
+                runCode();
+
+                // Every time the workspace changes state, save the changes to storage.
+                ws.addChangeListener((e) => {
+                    // UI events are things like scrolling, zooming, etc.
+                    // No need to save after one of these.
+                    if (e.isUiEvent) return;
+                    save(ws);
+                });
+
+                // Whenever the workspace changes meaningfully, run the code again.
+                ws.addChangeListener((e) => {
+                    // Don't run the code when the workspace finishes loading; we're
+                    // already running it once when the application starts.
+                    // Don't run the code during drags; we might have invalid state.
+                    if (
+                        e.isUiEvent ||
+                        e.type == Blockly.Events.FINISHED_LOADING ||
+                        ws.isDragging()
+                    ) {
+                        return;
+                    }
+                    runCode();
+                });
+            }
+        }
+
+        return () => {
+            mount.current = true;
+        }
+
+
+    }, []);
+
     return (
-        <Suspense fallback={<Loading />}>
-            {params.scenarioId}
-        </Suspense>
+        // <Suspense fallback={<Loading />}>
+        <div id="pageContainer">
+            <div id="outputPane">
+                <pre id="generatedCode"><code></code></pre>
+                <div id="output"></div>
+            </div>
+            <div id="blocklyDiv"></div>
+        </div>
+        // </Suspense>
         // <Room roomId={params.boardId} fallback={<Loading />}>
         //     <Canvas boardId={params.boardId} />
         //     <Chat roomId={params.boardId}/>
