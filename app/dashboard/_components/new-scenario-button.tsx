a600258deb1acb1767f90fc85eaea4f61093af9b
Đào Duy Thái <daoduythai.business@gmail.com>
2024-10-11 09:43:00 +0700

    create scenario successfully

--- app/dashboard/_components/new-scenario-button.tsx
+++ app/dashboard/_components/new-scenario-button.tsx
@@ -23,8 +23,6 @@ export const NewScenarioButton = ({
     // TODO: Create board
     // const { mutate, pending } = useApiMutation(api.board.create);
 
-    const [pending, setPending] = React.useState(true);
-
     const { user, isLoaded } = useUser();
 
     const images = [
@@ -63,46 +61,18 @@ export const NewScenarioButton = ({
                 ),
             });
             const data = await response.json();
-            // console.log(data);
-            // console.log(response)
             if (response.status === 200) {
                 toast.success(data.message);
             } else {
                 toast.error(data.title);
             }
-
-
         } catch (error) {
             console.error("Error creating project:", error);
         }
 
-        // mutate({
-        //     orgId,
-        //     title: "Untitled"
-        // })
-        //     .then((id) => {
-        //         toast.success("Board created")
-        //         //TODO: Redirect to /board/{id}
-        //     })
-        //     .catch(() => {
-        //         toast.error("Failed to create board")
-        //         onOpen();
-        //     })
     }
     return (
-        // <button disabled={disabled}
-        //     onClick={onClick}
-        //     className={cn(
-        //         "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6"
-        //         , (pending || disabled) && "opacity-75 hover:bg-blue-600 cursor-not-allowed")}
-        // >
-        //     <div />
-        //     <Plus className="h-12 w-12 text-white stroke-1" />
-        //     <p className="text-sm text-white font-light">
-        //         New board
-        //     </p>
-        // </button>
-        <button disabled={disabled}
+        <button
             onClick={onClick}
             className={cn(
                 "col-span-1 aspect-[100/127] bg-gray-500 rounded-lg hover:bg-gray-700 flex flex-col items-center justify-center py-6"
