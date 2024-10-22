3703f5ef7a7c33ed6cc1871e08e55aa1f82c62a2
Dao Duy Thai <daoduythai.business@gmail.com>
2024-10-22 11:15:29 +0700

    hot fix navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,12 @@
 import { UserButton, useOrganization } from "@clerk/nextjs";
 
 import React from "react";
+import { cn } from "@/lib/utils";
 import { SearchInput } from "../app/dashboard/_components/search-input";
+import { ProjectSelector } from "./project-selector";
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
 // import { SearchInput } from "./search-input";
 // import { InviteButton } from "./invite-button";
 
@@ -12,15 +17,30 @@ interface NavbarProps {
     searchEntity?: string;
 }
 
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"],
+});
+
 export const Navbar = (
     { searchLink, searchEntity }: NavbarProps
 ) => {
-    const { organization } = useOrganization();
     return (
-        <div className="flex items-center gap-x-4 p-5 ">
+        <div className="flex justify-between items-center w-full gap-x-4 p-5 ">
             <div className="hidden lg:flex-1 lg:flex ">
-                {/* TODO: Search Input */}
                 <SearchInput searchLink={searchLink} searchEntity={searchEntity} />
+            </div>
+            <Link className="lg:hidden block" href="/">
+                <div className="flex items-center gap-x-2">
+                    <Image priority src="/logo.svg" alt="logo" height={60} width={60} />
+                    <span className={cn(
+                        "font-semibold text-2xl",
+                        font.className,
+                    )}>INSYNC</span>
+                </div>
+            </Link>
+            <div className="w-fit lg:hidden block items-center justify-center">
+                <ProjectSelector />
             </div>
             <UserButton />
         </div>
--- components/project-sidebar.tsx
+++ components/project-sidebar.tsx
@@ -79,7 +79,7 @@ export const ProjectSidebar = () => {
         <div className=" hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
             <Link href="/">
                 <div className="flex items-center gap-x-2">
-                    <Image src="/logo.svg" alt="logo" height={60} width={60} />
+                    <Image priority src="/logo.svg" alt="logo" height={60} width={60} />
                     <span className={cn(
                         "font-semibold text-2xl",
                         font.className,
@@ -92,7 +92,7 @@ export const ProjectSidebar = () => {
                 </div>
             </Link>
 
-            
+
             <ProjectSelector/>
 
 
