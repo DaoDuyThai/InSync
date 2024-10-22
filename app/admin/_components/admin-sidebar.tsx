883f020da51203786e2d629b6a318691cd5037d1
Dao Duy Thai <daoduythai.business@gmail.com>
2024-10-22 17:01:41 +0700

    hotfix admin sidebar

--- app/admin/_components/admin-sidebar.tsx
+++ app/admin/_components/admin-sidebar.tsx
@@ -192,7 +192,7 @@ export function AdminSidebar() {
   const [selectedVersion, setSelectedVersion] = React.useState(data.versions[0])
 
   return (
-    <SidebarProvider>
+    
       <Sidebar>
         <SidebarHeader>
           <SidebarMenu>
@@ -286,33 +286,6 @@ export function AdminSidebar() {
         </SidebarContent>
         <SidebarRail />
       </Sidebar>
-      <SidebarInset>
-        <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          {Array.from({ length: 24 }).map((_, index) => (
-            <div
-              key={index}
-              className="aspect-video h-12 w-full rounded-lg bg-muted/50"
-            />
-          ))}
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+      
   )
 }
--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,9 @@
 // import { Navbar } from "@/components/navbar";
 // import { ProjectSidebar } from "@/components/project-sidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AdminSidebar } from "./_components/admin-sidebar"
+import { Separator } from "@/components/ui/separator"
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
 
 
 type Props = {
@@ -10,8 +12,26 @@ type Props = {
 
 const AdminLayout = ({ children }: Props) => {
     return (
+        <SidebarProvider>
+            <AdminSidebar />
+            <SidebarInset>
+                <header className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                </header>
 
-        <AdminSidebar />
+                <div className="flex flex-1 flex-col gap-4 p-4">
+                    
+                    {children}
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
+
+
+
+
+
+
 
     )
 }
