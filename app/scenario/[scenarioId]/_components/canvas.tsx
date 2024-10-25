f57d6d14ada49c792b4ad466f63ba0427848cd4f
Dao Duy Thai <daoduythai.business@gmail.com>
2024-10-25 16:31:43 +0700

    minor adjust in canvas

--- app/scenario/[scenarioId]/_components/canvas.tsx
+++ app/scenario/[scenarioId]/_components/canvas.tsx
@@ -5,6 +5,7 @@ import { save, load } from './serialization';
 import { toolbox } from './toolbox';
 import { jsonGenerator } from "./generators/json";
 import './blockly.css';
+import { toast } from "sonner";
 
 
 export const Canvas = () => {
@@ -16,9 +17,9 @@ export const Canvas = () => {
             Blockly.common.defineBlocks(blocks);
 
             // Set up UI elements and inject Blockly
-            const codeDiv = document.getElementById('generatedCode')?.firstChild;
+            // const codeDiv = document.getElementById('generatedCode')?.firstChild;
             const blocklyDiv = document.getElementById('blocklyDiv');
-            if (blocklyDiv && codeDiv) {
+            if (blocklyDiv) {
                 const workspace = Blockly.inject(blocklyDiv, {
                     toolbox,
                     scrollbars: false,
@@ -53,7 +54,11 @@ export const Canvas = () => {
                 // Function to generate and display the JSON code
                 const runCode = () => {
                     const code = jsonGenerator.workspaceToCode(workspace);
-                    (codeDiv as HTMLElement).innerText = code;
+                    if ((!code.startsWith('[') || !code.endsWith(']') ) && code.trim() !== '') {
+                        toast.error('Action blocks must be inside a scenario block')
+                    }
+                    console.log(code);
+                    // (codeDiv as HTMLElement).innerText = code;
                 };
 
                 // Load the initial state from storage and run the code
@@ -88,11 +93,11 @@ export const Canvas = () => {
         <div id="pageContainer" className="relative">
             <div id="blocklyDiv"></div>
             <div id="toolbox"></div>
-            <pre id="generatedCode" className="w-1/4  h-full"><code className=""></code></pre>
-            <div>
+            {/* <pre id="generatedCode" className="w-1/4  h-full"><code className=""></code></pre> */}
+            {/* <div>
                 <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="" />
                 <img src="https://gratisography.com/wp-content/uploads/2024/03/gratisography-funflower-800x525.jpg" alt="" />
-            </div>
+            </div> */}
         </div>
     );
 };
