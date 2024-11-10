c779f669be7c1ac0d02667bc8f74a6f78198ff76
Đào Duy Thái <daoduythai.business@gmail.com>
2024-11-11 00:01:16 +0700

    login authentication

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,25 @@
 import {
-    clerkMiddleware,
-    createRouteMatcher
+  clerkMiddleware,
+  createRouteMatcher
 } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
-    '/dashboard(.*)',
+  '/dashboard(.*)',
+  '/admin(.*)',
+  '/assets(.*)',
+  'logs(.*)',
+  'scenario(.*)',
 ]);
 
 export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
-  });
-  
-  export const config = {
-    matcher: [
-      // Skip Next.js internals and all static files, unless found in search params
-      '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-      // Always run for API routes
-      '/(api|trpc)(.*)',
-    ],
-  };
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
