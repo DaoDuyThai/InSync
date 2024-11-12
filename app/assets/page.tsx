1b703f9cddfc86f2167a299c1ce003736e39d6f4
Dương Thành Luân <103130587+LuanDTH0104@users.noreply.github.com>
2024-11-12 13:51:04 +0700

    Update page.tsx

--- app/assets/page.tsx
+++ app/assets/page.tsx
@@ -297,36 +297,6 @@ export default function ImagePage() {
                                     <circle cx="12" cy="12" r="10"></circle>
                                     <path d="M8 12h8"></path><path d="M12 8v8"></path>
                                 </svg>Add asset</button>
-                            <div
-                                role="tablist"
-                                aria-orientation="horizontal"
-                                className="inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground"
-                                tabIndex={0}
-                                data-orientation="horizontal"
-                                style={{ "outline": "none" }}>
-                                <button
-                                    type="button"
-                                    role="tab"
-                                    aria-selected="false"
-                                    aria-controls="radix-:r6j:-content-podcasts"
-                                    data-state="inactive" id="radix-:r6j:-trigger-podcasts"
-                                    className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow"
-                                    tabIndex={-1}
-                                    data-orientation="horizontal"
-                                    data-radix-collection-item="">Date</button>
-                                <button
-                                    type="button"
-                                    role="tab"
-                                    onClick={() => { setIsReverse(!isReverse) }}
-                                    aria-selected="true"
-                                    aria-controls="radix-:r6j:-content-music"
-                                    data-state="active" id="radix-:r6j:-trigger-music"
-                                    className="inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow relative"
-                                    tabIndex={0}
-                                    data-orientation="horizontal"
-                                    data-radix-collection-item="">{isReverse ? <ArrowDownWideNarrowIcon size={15} /> : <ArrowUpWideNarrowIcon size={15} />}</button>
-
-                            </div>
                         </div>
                         <div className="flex justify-center items-center h-[calc(100vh - 100px)]">
                             <div className="text-center">
