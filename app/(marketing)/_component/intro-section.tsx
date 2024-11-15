458c6a8031a945d676aa8a09f4c34c6c661db954
Đào Duy Thái <daoduythai.business@gmail.com>
2024-11-15 13:56:09 +0700

    top automation tool

--- app/(marketing)/_component/intro-section.tsx
+++ app/(marketing)/_component/intro-section.tsx
@@ -11,7 +11,7 @@ export default function IntroSection() {
             <div className="flex items-center justify-center flex-col">
                 <div className="mb-4 flex items-center border shadow-sm p-1 bg-amber-100 text-amber-700 rounded-full uppercase">
                     <Medal className="h-6 w-6 mr-2" />
-                    No 1 Automation Tool
+                    Top Automation Tool
                 </div>
                 <h1 className="text-3xl md:text-5xl text-center font-bold text-neutral-800 mb-6">
                     InSync - The ultimate tool for
